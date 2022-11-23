425a2f283536fbea27f46e507c3d3a6ec45205b2
unknown <tjiang@mit.edu>
2022-11-23 03:02:07 -0500

    Sheffe

--- Scheffes.py
+++ Scheffes.py
@@ -34,41 +34,65 @@ class Classifier(torch.nn.Module):
 
 
 
-def train(model, X, Y, criterion, batch_size=64, lr=0.001, epochs=1000):
+def train(model, X, Y, criterion, batch_size=64, lr=0.001, epochs=10000):
     """Label the items first
-        items in X have label 1, items in Y have label 0
+        items in X have label 0, items in Y have label 1
         then train the model with sgd
+        X, Y are numpy arrays
+        X has shape (N1, 2), Y has shape (N2, 2)
     """
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     for epoch in range(epochs):
-        print("epoch: ", epoch)
         for i in range(0, len(X), batch_size):
             optimizer.zero_grad()
             X_batch = X[i:i+batch_size]
             Y_batch = Y[i:i+batch_size]
-            X_batch = X_batch.to(device)
-            Y_batch = Y_batch.to(device)
-            output_X = model(X_batch)
-            output_Y = model(Y_batch)
-            loss_X = criterion(output_X, torch.ones(len(X_batch), 1).to(device))
-            loss_Y = criterion(output_Y, torch.zeros(len(Y_batch), 1).to(device))
+            X_batch = torch.from_numpy(X_batch).float()
+            Y_batch = torch.from_numpy(Y_batch).float()
+            output_X = model(X_batch.to(device)).to(device)
+            output_Y = model(Y_batch.to(device)).to(device)
+            loss_X = criterion(output_X, torch.zeros(len(X_batch)).long().to(device))
+            loss_Y = criterion(output_Y, torch.ones(len(Y_batch)).long().to(device))
             loss = loss_X + loss_Y
             loss.backward()
             optimizer.step()
+        if epoch % 100 == 0:
+            print("loss: ", loss)
+            print("accuracy on X: ", inference(model, len(X), X))
+    raise Exception("Done training")
+    return model
     
 
-def inference(model,size_m, parameters, trial=1000):
-    '''
-    for t in range(trial):
-        Z, _ = gen_data(size_m, parameters)
+def inference(model,size_m, Z, target_label=0):
+    '''Inference on Z
+        make sure no gradient is computed
+        z is m pieces of data
         Z = Z.to(device)
-        class=torch.sum(model(Z))
-        success+=(class.item()==0)
+        class=torch.sum(argmax(model(Z)))
+    compute probability of data in z be 0
     '''
-    pass
-
+    with torch.no_grad():
+        success = 0
+        if True:
+            Z = torch.from_numpy(Z).float()
+            class_ = torch.argmax(model(Z.to(device)), dim=1)
+            success += torch.sum(class_ == target_label)
+        return success/size_m
 
 if __name__ == "__main__":
+    sigma_mx_2_standard = np.array([[0.03, 0], [0, 0.03]])
+    sigma_mx_2 = np.zeros([9,2,2])
+    for i in range(9):
+        sigma_mx_2[i] = sigma_mx_2_standard
+        if i < 4:
+            sigma_mx_2[i][0 ,1] = -0.02 - 0.002 * i
+            sigma_mx_2[i][1, 0] = -0.02 - 0.002 * i
+        if i==4:
+            sigma_mx_2[i][0, 1] = 0.00
+            sigma_mx_2[i][1, 0] = 0.00
+        if i>4:
+            sigma_mx_2[i][1, 0] = 0.02 + 0.002 * (i-5)
+            sigma_mx_2[i][0, 1] = 0.02 + 0.002 * (i-5)
     device = torch.device("cuda")
     try:
         title=sys.argv[1]
@@ -76,10 +100,22 @@ if __name__ == "__main__":
         title='untitled_run'
     x_in=2
     H=50
-    model=Classifier(x_in, H)
+    model=Classifier(x_in, H).to(device)
     criterion = torch.nn.CrossEntropyLoss().cuda()
     n=100
-    sigma_mx_2=np.ones((3,3))
+    m=50
     X, Y=sample_blobs_Q(n, sigma_mx_2)
     model=train(model, X, Y, criterion)
+    total=0
+    for trial in range(1000):
+        Z, _ = sample_blobs_Q(m, sigma_mx_2)
+        success = inference(model, m, Z, target_label=0)
+        total += success
+    print("average success rate of class 0: ", total/1000)
+    total=0
+    for trial in range(1000):
+        _, Z = sample_blobs_Q(m, sigma_mx_2)
+        success = inference(model, m, Z, target_label=1)
+        total += success
+    print("average success rate of class 1: ", total/1000)
     
