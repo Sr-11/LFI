505a0f7f1acb0de06fe4e665a8ac2706f4a69a90
unknown <tjiang@mit.edu>
2023-01-04 00:00:48 -0500

    somehow...

--- LFI.py
+++ LFI.py
@@ -134,7 +134,7 @@ def train_d(n_list, m_list, N_per=100, title='Default', learning_rate=5e-4, K=15
                     if LfI:
                         S=MatConvert(np.concatenate(([x, y, z]), axis=0), device, dtype)
                         Fea=model_u(S)
-                        mmd_squared_temp, mmd_squared_var_temp=MMD_LFI_STAT(Fea, S, batch_size, batch_m, sigma=sigma, cst=cst).to(device)
+                        mmd_squared_temp, mmd_squared_var_temp=MMD_LFI_STAT(Fea, S, batch_size, batch_m, sigma=sigma, cst=cst)
                         STAT_u = torch.sub(mmd_squared_temp, relu(mmd_squared_var_temp), alpha=1.0)
                     J_star_u[kk, t] = STAT_u.item()
                     optimizer_u.zero_grad()
