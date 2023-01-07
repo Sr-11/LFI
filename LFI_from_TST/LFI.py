ae9286000e04184980e97824cd7615127233c532
unknown <tjiang@mit.edu>
2023-01-07 03:29:10 -0500

    edits

--- LFI_from_TST/LFI.py
+++ LFI_from_TST/LFI.py
@@ -8,19 +8,19 @@ import pickle
 from Data_gen import *
 
 # load dataset 
-diffusion = np.load("../Diffusion/ddpm_generated_images.npy")
-try:
-    trainset = datasets.CIFAR10(root='./data', train=True, download=False)
-    #testset = datasets.CIFAR10(root='./data', train=False, download=False)
-except:
-    trainset = datasets.CIFAR10(root='./data', train=True, download=True)
-    #testset = datasets.CIFAR10(root='./data', train=False, download=True)
-cifar10 = np.zeros((50000,32,32,3))
-for i in range(50000):
-    cifar10[i] = np.asarray(trainset[i][0])
+def load_diffusion_cifar():
+    diffusion = np.load("../Diffusion/ddpm_generated_images.npy")
+    try:
+        trainset = datasets.CIFAR10(root='../data', train=True, download=False)
+    except:
+        trainset = datasets.CIFAR10(root='../data', train=True, download=True)
+    cifar10 = np.zeros((50000,32,32,3))
+    for i in range(50000):
+        cifar10[i] = np.asarray(trainset[i][0])
 
-dataset_P = diffusion.reshape(diffusion.shape[0], -1)
-dataset_Q = cifar10.reshape(cifar10.shape[0], -1)
+    dataset_P = diffusion.reshape(diffusion.shape[0], -1)
+    dataset_Q = cifar10.reshape(cifar10.shape[0], -1)
+    return dataset_P, dataset_Q
 
 class ModelLatentF(torch.nn.Module):
     """Latent space for both domains."""
@@ -210,6 +210,7 @@ if __name__ == "__main__":
 
     diffusion_data=True
     if diffusion_data:
+        dataset_P, dataset_Q = load_diffusion_cifar()
         def diffusion_cifar10(n):
             if n <0 :
                 return 'DIFFUSION'            
