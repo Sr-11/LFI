fb0b2bd4773b425c284997b8e8329c2924d1c1e4
Sr-11 <eruisun@mit.edu>
2023-01-18 00:50:46 -0500

    edit

--- LFI_CIFAR/Scheffes_v1.py
+++ LFI_CIFAR/Scheffes_v1.py
@@ -6,20 +6,30 @@ from tqdm import trange
 from utils import *
 from matplotlib import pyplot as plt
 import torch.nn as nn
+dtype = torch.float
 device = torch.device("cuda")
+seed = 42
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+np.random.seed(seed)
+torch.backends.cudnn.deterministic=True
 criterion = torch.nn.CrossEntropyLoss().cuda()
 def load_diffusion_cifar_32():
     diffusion = np.load("../Diffusion/ddpm_generated_images2.npy").transpose(0,3,1,2)
     cifar10 = np.load('../data/cifar_data.npy')
     dataset_P = diffusion.reshape(diffusion.shape[0], -1)
     dataset_Q = cifar10.reshape(cifar10.shape[0], -1)
-    return dataset_Q, dataset_P
+    return dataset_P, dataset_Q
 
 DP, DQ = load_diffusion_cifar_32()
 DP1 = DP[:4000, :]
 DQ1 = DQ[:4000, :]
 DP2 = DP[4000:13140, :]
 DQ2 = DQ[4000:13140, :]
+DP1_T = torch.tensor(DP1, dtype=dtype, device=device)
+DQ1_T = torch.tensor(DQ1, dtype=dtype, device=device)
+DP2_T = torch.tensor(DP2, dtype=dtype, device=device)
+DQ2_T = torch.tensor(DQ2, dtype=dtype, device=device)
 def gen_fun1(n):
     X = DP1[np.random.choice(DP1.shape[0], n, replace=False), :]
     Y = DQ1[np.random.choice(DQ1.shape[0], n, replace=False), :]
@@ -28,7 +38,14 @@ def gen_fun2(n):
     X = DP2[np.random.choice(DP2.shape[0], n, replace=False), :]
     Y = DQ2[np.random.choice(DQ2.shape[0], n, replace=False), :]
     return X, Y
-
+def gen_fun1_T(n):
+    X = DP1_T[np.random.choice(DP1_T.shape[0], n, replace=False), :]
+    Y = DQ1_T[np.random.choice(DQ1_T.shape[0], n, replace=False), :]
+    return X, Y
+def gen_fun2_T(n):
+    X = DP2_T[np.random.choice(DP2_T.shape[0], n, replace=False), :]
+    Y = DQ2_T[np.random.choice(DQ2_T.shape[0], n, replace=False), :]
+    return X, Y
 class ConvNet_CIFAR10(nn.Module):
     def __init__(self):
         super(ConvNet_CIFAR10, self).__init__()
@@ -116,8 +133,6 @@ def likelihood(model, Z):
     output_Z = model(Z.to(device)) #has shape (batch_size, 2)
     prob = torch.softmax(output_Z, dim=1)[:,0] #Probability of class 0
     return np.mean(prob.detach().cpu().numpy())
-
-
 def Scheffe(model,Z):
     '''
     Output number of Z's classified as class 0
@@ -128,62 +143,102 @@ def Scheffe(model,Z):
         class_ = torch.argmax(model(Z.to(device)), dim=1)
         success = torch.sum(class_ == 0).float() / len(Z)
         return float(success)
+def Scheffe_T(model, Z_T):
+    '''
+    Output number of Z's classified as class 0 but Z is already a tensor
+    '''
+    with torch.no_grad():
+        success = 0
+        class_ = torch.argmax(model(Z_T.to(device)), dim=1)
+        success = torch.sum(class_ == 0).float() / len(Z_T)
+        return float(success)
+def likelihood_T(model, Z_T):
+    '''
+    Output number of Z's classified as class 0 but Z is already a tensor
+    '''
+    with torch.no_grad():
+        output_Z = model(Z_T.to(device)) #has shape (batch_size, 2)
+        prob = torch.softmax(output_Z, dim=1)[:,0] #Probability of class 0
+        return np.mean(prob.detach().cpu().numpy())
+    
 
 n=256
-m_list = list(range(4, 180, 4))
+m_list = list(range(4,64, 4))
+for k in range(10):
+        F1 = './data/RRresult_SCH_'+str(n)+'_'+str(k)+'_.txt'
+        F2 = './data/RRresult_LBI_'+str(n)+'_'+str(k)+'_.txt'
 
-F1 = './data/result_SCH_'+str(n)+'_.txt'
-F2 = './data/result_LBI_'+str(n)+'_.txt'
+        fwrite('', F1, message='New File '+str(n))
+        fwrite('', F2, message='New File '+str(n))
+        if True:
+            X, Y = gen_fun1(n)
+            model=train(X, Y, criterion, batch_size=32, lr=0.0001, epochs=100)
 
-fwrite('', F1, message='New File '+str(n))
-fwrite('', F2, message='New File '+str(n))
-if True:
-    X, Y = gen_fun1(n)
-    model=train(X, Y, criterion, batch_size=32, lr=0.0001, epochs=300)
-
-    for m in m_list:
-        H=[[], []]
-        for i in range(1000):
-            X1, Y1 = gen_fun1(m)
-            H[0].append(Scheffe(model, X1))
-        thres = np.percentile(H[0], 5)
-        H=[[], []]
-        T=[[], []]
-        for i in range(1000):
-            X1, Y1 = gen_fun1(m)
-            H[0].append(Scheffe(model, X1)>=thres)
-            H[1].append(Scheffe(model, Y1)<thres)
-            T[0].append(Scheffe(model, X1)>0.5)
-            T[1].append(Scheffe(model, Y1)<0.5)
-        st1="n, m= "+str(n)+str('  ')+str(m)+" --- P(max|Z~X):  "+str(np.mean(T[0]))
-        st2="n, m= "+str(n)+str('  ')+str(m)+" --- P(max|Z~Y):  "+str(np.mean(T[1]))
-        st3="n, m= "+str(n)+str('  ')+str(m)+" --- P(95|Z~X):  "+str(np.mean(H[0]))
-        st4="n, m= "+str(n)+str('  ')+str(m)+" --- P(95|Z~Y):  "+str(np.mean(H[1]))
-        fwrite(st1, F1)
-        fwrite(st2, F1)
-        fwrite(st3, F1)
-        fwrite(st4, F1)
-    print('Done with Scheffe')
-    for m in m_list:
-        H=[[], []]
-        for i in range(1000):
-            X1, Y1 = gen_fun1(m)
-            H[0].append(likelihood(model, X1))
-        thres = np.percentile(H[0], 5)
-        H=[[], []]
-        T=[[], []]
-        for i in range(1000):
-            X1, Y1 = gen_fun1(m)
-            H[0].append(likelihood(model, X1)>=thres)
-            H[1].append(likelihood(model, Y1)<thres)
-            T[0].append(likelihood(model, X1)>0.5)
-            T[1].append(likelihood(model, Y1)<0.5)
-        st1="n, m= "+str(n)+str('  ')+str(m)+" --- P(max|Z~X):  "+str(np.mean(T[0]))
-        st2="n, m= "+str(n)+str('  ')+str(m)+" --- P(max|Z~Y):  "+str(np.mean(T[1]))
-        st3="n, m= "+str(n)+str('  ')+str(m)+" --- P(95|Z~X):  "+str(np.mean(H[0]))
-        st4="n, m= "+str(n)+str('  ')+str(m)+" --- P(95|Z~Y):  "+str(np.mean(H[1]))
-        fwrite(st1, F2)
-        fwrite(st2, F2)
-        fwrite(st3, F2)
-        fwrite(st4, F2)
-    print('Done with likelihood')
+            for m in m_list:
+                H=[]
+                for i in range(1000):
+                    X1, Y1 = gen_fun1(m)
+                    H.append(Scheffe(model, X1))
+                stat = np.sort(H)
+                thres = stat[49]
+                #thres = np.percentile(H, 5)
+                H=[[], []]
+                T=[[], []]
+                P=[[], []]
+                for i in range(1000):
+                    X1, Y1 = gen_fun1(m)
+                    tx = Scheffe(model, X1)
+                    ty = Scheffe(model, Y1)
+                    H[0].append(tx>=thres)
+                    H[1].append(ty<thres)
+                    T[0].append(tx>0.5)
+                    T[1].append(ty<0.5)
+                    P[0].append(np.searchsorted(stat, tx)/1000.0)
+                    P[1].append(np.searchsorted(stat, ty)/1000.0)
+                st1="n, m= "+str(n)+str('  ')+str(m)+" --- P(max|Z~X):  "+str(np.mean(T[0]))
+                st2="n, m= "+str(n)+str('  ')+str(m)+" --- P(max|Z~Y):  "+str(np.mean(T[1]))
+                st3="n, m= "+str(n)+str('  ')+str(m)+" --- P(95|Z~X):  "+str(np.mean(H[0]))
+                st4="n, m= "+str(n)+str('  ')+str(m)+" --- P(95|Z~Y):  "+str(np.mean(H[1]))
+                st5="n, m= "+str(n)+str('  ')+str(m)+" --- P(p|Z~X):  "+str(1-np.mean(P[0]))
+                st6="n, m= "+str(n)+str('  ')+str(m)+" --- P(p|Z~Y):  "+str(1-np.mean(P[1]))
+                fwrite(st1, F1)
+                fwrite(st2, F1)
+                fwrite(st3, F1)
+                fwrite(st4, F1)
+                fwrite(st5, F1)
+                fwrite(st6, F1)
+            print('Done with Scheffe')
+            for m in m_list:
+                H=[]
+                for i in range(1000):
+                    X1, Y1 = gen_fun1(m)
+                    H.append(likelihood(model, X1))
+                #thres = np.percentile(H, 5)
+                stat = np.sort(H)
+                thres = stat[49]
+                H=[[], []]
+                T=[[], []]
+                P=[[], []]
+                for i in range(1000):
+                    X1, Y1 = gen_fun1(m)
+                    tx = likelihood(model, X1)
+                    ty = likelihood(model, Y1)
+                    H[0].append(tx>=thres)
+                    H[1].append(ty<thres)
+                    T[0].append(tx>0.5)
+                    T[1].append(ty<0.5)
+                    P[0].append(np.searchsorted(stat, tx)/1000.0)
+                    P[1].append(np.searchsorted(stat, ty)/1000.0)                
+                st1="n, m= "+str(n)+str('  ')+str(m)+" --- P(max|Z~X):  "+str(np.mean(T[0]))
+                st2="n, m= "+str(n)+str('  ')+str(m)+" --- P(max|Z~Y):  "+str(np.mean(T[1]))
+                st3="n, m= "+str(n)+str('  ')+str(m)+" --- P(95|Z~X):  "+str(np.mean(H[0]))
+                st4="n, m= "+str(n)+str('  ')+str(m)+" --- P(95|Z~Y):  "+str(np.mean(H[1]))
+                st5="n, m= "+str(n)+str('  ')+str(m)+" --- P(p|Z~X):  "+str(1-np.mean(P[0]))
+                st6="n, m= "+str(n)+str('  ')+str(m)+" --- P(p|Z~Y):  "+str(1-np.mean(P[1]))
+                fwrite(st1, F2)
+                fwrite(st2, F2)
+                fwrite(st3, F2)
+                fwrite(st4, F2)
+                fwrite(st5, F2)
+                fwrite(st6, F2)
+            print('Done with likelihood')
